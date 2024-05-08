cf98acaff318118700296eb66256beb24f973161
Grazielly Varanda <vieiravarandagrazielly@gmail.com>
2024-05-08 20:50:30 -0300

    feat(web): create name form

---
+++ apps/web/nameForm.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jogo de Multiplicação</title>
+    <link rel="stylesheet" href="nameForm.css">
+</head>
+<body>  
+    <div class="container">
+        <h1>Jogo de Multiplicação</h1>
+        <form id="nameForm">
+                <label for="nome">Por favor, insira seu nome:</label>
+                <input type="text" id="nome" name="nome" maxlength="50" required>
+                <button onclick="generateRandomNumber()"> <a href="index.php" > Iniciar Jogo </a> </button>
+        </form> 
+    </div>
+    <script src="script.js"></script>
+</body>
+</html>   
+
+
+
+
